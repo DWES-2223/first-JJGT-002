20d24f69ac8633024dc2f36403eba1ea6a980642
jorge <jjlosantos7@gmail.com>
2023-10-01 21:27:07 +0200

    [ADD] 270a.php

--- php/src/entregables/270.php
+++ php/src/entregables/270.php
@@ -7,35 +7,7 @@
 </head>
 <body>
     <?php
-    $records = include "./atletes.php";
-    var_dump($records);
+    include "270a.php";
     ?>
-    <table>
-        <tr>
-            <th>Prova</th>
-            <th>Marca</th>
-            <th>Atleta</th>
-            <th>Natalici</th>
-            <th>Club</th>
-            <th>Data</th>
-            <th>Lloc</th>
-        </tr>
-        <?php
-        foreach ($records as $nombreRecord => $record) {
-            ?>
-            <tr>
-                <td><?=$nombreRecord?></td>
-                <?php
-                foreach ($record as $especificacion) {
-                    ?>
-                    <td><?=$especificacion?></td>
-                    <?php
-                }
-                ?>
-            </tr>
-            <?php
-        }
-        ?>
-    </table>
 </body>
 </html>
